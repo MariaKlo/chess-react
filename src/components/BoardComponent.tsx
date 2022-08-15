6eca4119428f08c328963d1ffc051ec3707180a2
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-08-15 20:34:54 +0300

    Initialize board and cells

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Board } from '../models/Board';
+import { Cell } from '../models/Cell';
+import CellComponent from './CellComponent';
 
-const BoardComponent = () => {
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = (board, setBoard) => {
   return (
     <div className='board'>
-
+      {board.cells.map((row: Cell[], index: number) =>
+        <React.Fragment key={index}>
+          {row.map(cell =>
+          <CellComponent
+            cell={cell}
+            key={cell.id}
+          />
+        )}
+      </React.Fragment>
+      )}
     </div>
   );
 };
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Cell } from '../models/Cell';
+
+interface CellProps {
+  cell: Cell;
+}
 
 const CellComponent = () => {
   return (
-    <div className='cell'>
+    <div className={['cell', cell.color].join(' ')}>
 
     </div>
   );
